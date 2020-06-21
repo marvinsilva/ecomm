781f7305c3ce068e6ec7f1bf390650a326d2c317
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-06-21 19:16:37 -0300

    Implemented update and delete products functions and pages

--- ecomm/products/urls.py
+++ ecomm/products/urls.py
@@ -5,4 +5,6 @@ from ecomm.products import views
 urlpatterns = [
     path('list', views.list_products, name='list'),
     path('create', views.create_product, name='create'),
+    path('delete/<int:product_id>', views.delete_product, name='delete'),
+    path('update/<int:product_id>', views.update_product, name='update'),
 ]
--- ecomm/products/views.py
+++ ecomm/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from ecomm.products.forms import ProductModelForm
 from ecomm.products.models import Product
@@ -9,6 +9,7 @@ def list_products(request):
 
     context = {
         'products': products,
+        'products_empty': []
     }
 
     return render(request, 'products/list.html', context=context)
@@ -30,3 +31,35 @@ def create_product(request):
         'form': form
     }
     return render(request, 'products/create.html', context=context)
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == "POST":
+        product.delete()
+        return redirect('products:list')
+    context = {
+        "product": product
+    }
+    return render(request, "products/delete.html", context)
+
+
+def update_product(request, product_id):
+    product = Product.objects.get(pk=product_id)
+
+    if request.method == 'POST':
+        # salvar form
+        form = ProductModelForm(data=request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('products:list')
+    else:
+        # GET
+        form = ProductModelForm(instance=product)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'products/update.html', context=context)
