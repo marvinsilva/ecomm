71471f6d8572512c5683106730d08d1a50d5a158
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-06-21 19:26:12 -0300

    Created a middleware for a request method result

---
+++ ecomm/products/middlewares.py
@@ -0,0 +1,12 @@
+def aceleradev_middleware(get_response):
+
+    def middleware(request):
+        if request.method == 'GET':
+            print('Método da requisição é GET')
+        print('='*10)
+        print('Acelera Dev')
+        response = get_response(request)
+        print("Online na Codenation")
+        return response
+
+    return middleware
--- ecomm/settings.py
+++ ecomm/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '*6snp37=-7!ubmfoj^^dpwk8$cyd3^ed(^!(!3!j)^87(0qr-1'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -48,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'ecomm.products.middlewares.aceleradev_middleware'
 ]
 
 ROOT_URLCONF = 'ecomm.urls'
@@ -70,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecomm.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -80,7 +78,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -100,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -113,7 +109,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
