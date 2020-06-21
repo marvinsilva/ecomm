d7d4debffcd704d9600c0423fa53d6b0992e29e5
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-06-21 18:25:48 -0300

    Implemented create nem products function and page

--- ecomm/products/urls.py
+++ ecomm/products/urls.py
@@ -4,4 +4,5 @@ from ecomm.products import views
 
 urlpatterns = [
     path('list', views.list_products, name='list'),
+    path('create', views.create_product, name='create'),
 ]
--- ecomm/products/views.py
+++ ecomm/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 
+from ecomm.products.forms import ProductModelForm
 from ecomm.products.models import Product
 
 
@@ -11,3 +12,21 @@ def list_products(request):
     }
 
     return render(request, 'products/list.html', context=context)
+
+
+def create_product(request):
+    if request.method == 'POST':
+        # Salvar form
+        form = ProductModelForm(request.POST)
+        if form.is_valid():
+            # True -> é valido
+            form.save()
+            return redirect('products:list')
+    else:
+        # Get form
+        form = ProductModelForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'products/create.html', context=context)
